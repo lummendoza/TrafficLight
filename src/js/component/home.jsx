3345db0a140b123c25d40c3a7891b24d9bd826cb
lummendoza <luismiguelmm95@gmail.com>
2023-02-25 00:13:58 +0000

    Need to add the functionaliti

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	// const colors = ["red","yellow","green"];
+	const [ color, setColor] = useState("selected"); 
+	
 	return (
 		<div className="d-flex flex-column align-items-center">
 			<div className="traffic-pole bg-dark">
-				<p>hola</p>
-
-
+				
 			</div>
 			<div className="traffic-box-ligth rounded d-flex flex-column align-items-center justify-content-center">
 				
-				<div className="light red m-1">
-					<p>hola</p>
+				<div className="light red selected m-1 ">
+					
 				</div>
-				<div className="light yellow m-1">
-					<p>hola</p>
+				<div className={"light yellow m-1"} onClick={() => ("selected")}>
+					
 				</div>
 				<div className="light green m-1">
-					<p>hola</p>
+					
 				</div>
 
 			</div>
